67078fdc40802b510a137d8f68d8fc7917838583
purka <malyarchyk.17@gmail.com>
2014-10-25 12:10:23 +0300

    создан класс crop и четрые вида cropa

--- cell.js
+++ cell.js
@@ -1,30 +1,13 @@
 Cell = function() {
 	this.crop = null;
-	this.stage = 0;
 
 	this.plant = function(crop) {
 		console.log("Высадили "+crop);
 		this.crop = crop;
-		this.grow();
+		this.crop.grow();
 	}
-	this.grow = function() {
-		this.stage++;
-		console.log(this);
-		console.log("стадия роста - "+this.stage)
-		if (this.stage >= 4) {
-			console.log(this.crop+" вырос и собран");
-			this.harvest();
-		} else {
-			var cell = this;
-			setTimeoutSecond(function(){cell.grow()}, 5);
-		}
-	}
-	this.harvest = function() {
-		this.crop = null;
-		this.stage = 0;
-	}
+
 };
 
-test_cell = new Cell();
-test_cell.plant('картоха')
-
+test = new Cell()
+test.plant(cat)
