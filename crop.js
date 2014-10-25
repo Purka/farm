3ad852981327e16e050b00fca86870791cf7ea38
purka <malyarchyk.17@gmail.com>
2014-10-25 18:14:34 +0300

    image

--- crop.js
+++ crop.js
@@ -1,7 +1,8 @@
-Crop = function(name){
+Crop = function(name, image){
 	this.stage = 0;
 	this.name = name;
 	this.cell = null;
+	this.image = image;	
 	
 	this.grow = function() {
 		this.stage++;
@@ -22,8 +23,8 @@ Crop = function(name){
 
 }
 
-var potato = new Crop('potato')
-var banana = new Crop('banana')
-var wheat = new Crop('wheat')
-var cat = new Crop('cat')
+var potato = new Crop('potato', 'http://fermaspb.ru/uploads/.thumbs/images/goods/res-4d43b90e63868.jpg')
+var banana = new Crop('banana', 'http://www.pl.all.biz/img/pl/catalog/middle/199459.jpeg?rrr=1')
+var wheat = new Crop('wheat', 'http://images.tomas.by/i/firms/49/23/23585/pshenica-dlya-prorashchivaniya-0-5-kg_074050d285145c0_100x100.jpg')
+var cat = new Crop('cat', 'http://img-fotki.yandex.ru/get/9317/27433797.89/0_8d647_1716eb7d_-1-orig')
 
