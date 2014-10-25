04b46d4e5815c9d6abbcb221610b427f25f71ed7
AlexeyRase <yaltarase@gmail.com>
2014-10-25 18:18:33 +0300

    menu

--- storage.js
+++ storage.js
@@ -1,14 +1,23 @@
-storage = {
+Storage = function() {
+	this.crops = {
 		'potato': 1,
 		'banana': 1,
 		'wheat' : 1,
 		'cat' : 1
-};
-
-draw_indicators = function() {
-	var code ='';
-	$.each(storage, function(resource, value) {
-		code += '<span class="indicator">'+resource+": "+value+'</span>';
-	})
-	$(".indicators").html(code)
+	};
+	this.drawIndicators = function() {
+		var code ='';
+		$.each(this.crops, function(resource, value) {
+			code += '<span class="indicator">'+resource+": "+value+'</span>';
+		});
+		$(".indicators").html(code);
+	};
+	this.drawMenu = function() {
+		$.each(this.crops, function(resource, value) {
+			var code = '';
+			code += (value>0?'<img class="resource url =">'+value+'</span>':'');
+			$(".menu").html(code);
+		});
+	}
 }
+
