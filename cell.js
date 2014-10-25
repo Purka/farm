c9e2dc5e94fa1cc28d17addbacb33a7e8933a019
purka <malyarchyk.17@gmail.com>
2014-10-25 17:40:22 +0300

    change storage

--- cell.js
+++ cell.js
@@ -4,17 +4,17 @@ Cell = function(x, y) {
 	this.y = y;
 	
 	this.plant = function(crop) {
-		if(this.crop || storage['potato'] <= 0){return null};
+		if(this.crop || storage[crop.name] <= 0){return null};
 		this.crop = clone(crop);
-		storage['potato']--;
+		storage[crop.name]--;
 		console.log("Высадили "+this.crop.name+' на '+this.x+';'+this.y);
 		this.crop.cell = this;
 		this.crop.grow();
 	};
 	this.harvest = function() {
 		console.log('harvest')
-		storage['potato'] +=3;
-		console.log("Теперь у тебя " + storage['potato'] + " " + this.crop.name);
+		storage[this.crop.name] +=3;
+		console.log("Теперь у тебя " + storage[this.crop.name] + " " + this.crop.name);
 		delete this.crop;
 		this.draw();
 	};
